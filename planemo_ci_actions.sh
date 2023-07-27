af38047a708b163de456aaa1fca9881ea4598264
Matthias Bernt <m.bernt@ufz.de>
2023-07-27 21:18:52 +0200

    Revert "force and redirect"
    This reverts commit 063aac3f36e0b72604ef3bb0a51448ff9ce718d1.

--- planemo_ci_actions.sh
+++ planemo_ci_actions.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 # function running an infinite loop pruning (unused) docker images
 function background_prune_docker {
   while 1; do
-    docker system prune --all --volumes --force
+    docker system prune --all --volumes
     sleep 60
   done
 }
@@ -161,7 +161,7 @@ if [ "$MODE" == "test" ]; then
   # show tools
   cat tool_list_chunk.txt
 
-  background_prune_docker &> /dev/null &
+  background_prune_docker &
   PRUNE_PID=$!
 
   # Test tools
