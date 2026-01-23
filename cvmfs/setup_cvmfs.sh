13ce3c3f235ca8d11633a04fb77e130103d21fff
Marius van den Beek <m.vandenbeek@gmail.com>
2026-01-23 10:17:12 +0100

    Update cvmfs repo location (#66)

--- cvmfs/setup_cvmfs.sh
+++ cvmfs/setup_cvmfs.sh
@@ -4,7 +4,7 @@ set -x
 cd "${BASH_SOURCE%/*}/" || exit
 
 sudo apt install lsb-release
-wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
+wget https://cvmrepo.s3.cern.ch/cvmrepo/apt/cvmfs-release-latest_all.deb
 sudo dpkg -i cvmfs-release-latest_all.deb
 sudo apt-get update
 sudo apt install cvmfs cvmfs-config
